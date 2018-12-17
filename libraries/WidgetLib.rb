621e8bb8abfe5e331bb86751b4240715207a26b2
Eric Klavins <klavins@uw.edu>
2018-12-17 15:11:29 -0800

    Aquadoc initial commit

---
+++ libraries/WidgetLib.rb
@@ -0,0 +1,11 @@
+# A module with very little in it, used to test Aquarium's import/export features.
+module WidgetLib
+    
+    # Show the user a page that says thanks!
+    def say_thanks
+        show do
+            title "Thanks!"
+        end
+    end
+    
+end
